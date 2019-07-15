d096362baf8d6008d6999ec1598e80c5669a7ce4
javedalamthabrealam <51222868+javedalamthabrealam@users.noreply.github.com>
2019-07-15 15:07:54 +0530

    Create jha.py

---
+++ jha.py
@@ -0,0 +1,50 @@
+a,b = map(str,input().split())
+m = 0
+if len(a)>len(b):
+	a,b=b,a
+x = 0
+while x<len(a):
+	  m += (ord(b[x])-ord(a[x]))
+	  x += 1
+for x in range(x,len(b)):
+	  m += ord(b[x])-ord('a')+1
+print(m)
+sadvsdvsd
+vds
+vdsv
+dv
+sdv
+d
+vsd
+vd
+vd
+vd
+v
+dv
+dv
+sd
+vsdv
+dv
+dv
+sd
+vsd
+v
+sdv
+sdv
+sdv
+d
+vd
+v
+dv
+d
+vd
+vsd
+vd
+v
+dv
+dv
+d
+vdv
+dv
+sdvsd
+v
