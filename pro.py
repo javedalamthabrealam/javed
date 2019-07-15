a03a800746d4763a6a2c2e1bd9eeffc6c0c2afc8
javedalamthabrealam <51222868+javedalamthabrealam@users.noreply.github.com>
2019-07-15 15:01:49 +0530

    Create pro.py

---
+++ pro.py
@@ -0,0 +1,14 @@
+m,n=input().strip().split(" ")
+n=int(n)
+s=0
+while s<len(m)-1 and n:
+	if(m[s]>m[s+1]):
+		n-=1
+		m=m[:s]+m[s+1:]
+		if(s!=0):
+			s-=1
+	else:
+		s+=1
+q=m[:len(m)-n]
+print(q)
+gduyvgfevfhuvgevefefefhyweergeryvgfv 7
