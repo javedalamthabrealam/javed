a8322ed4d1ed65c323b25754568c0b05ad7f8b49
javedalamthabrealam <51222868+javedalamthabrealam@users.noreply.github.com>
2019-07-15 14:37:43 +0530

    Update vow.py

--- vow.py
+++ vow.py
@@ -2,6 +2,6 @@ n = raw_input()
 if(n=='a' or n=='e' or n=='i' or n=='o' or n=='u'):
     print("vowel")
 elif(n>=0 or n<=9):
-    print("invalid")
+    print("Consonant")
 else:
     print("consonant")
