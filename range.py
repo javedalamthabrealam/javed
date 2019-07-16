9831603f488e22256c2cacb868c80e0c66da616e
javedalamthabrealam <51222868+javedalamthabrealam@users.noreply.github.com>
2019-07-16 12:21:55 +0530

    Create range.py

---
+++ range.py
@@ -0,0 +1,15 @@
+n=int(input())
+l=[]
+m=len(str(n))
+s=0
+for _ in range(m):
+	s+=9
+for i in range(n-s,n):
+	r=0
+	d=i
+	while(d>0):
+		r+=(d%10)
+		d=d//10
+	if(r+i==n):
+		l.append(i)
+print(len(1),*1,sep='\n')
