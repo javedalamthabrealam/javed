5b54e397ec57821154caa3be98902295228d6866
javedalamthabrealam <51222868+javedalamthabrealam@users.noreply.github.com>
2019-07-16 14:37:10 +0530

    Update n1.py

--- n1.py
+++ n1.py
@@ -6,6 +6,6 @@ if n1==1:
   
 cnt=0
 for i in range(1,n1-1):
-  if(l[i]>l[i-1] and (l[i]>l[i+1] or l[i]<l[i-1] and l[i]<l[i+1]))
+  if((l[i]>l[i-1] and (l[i]>l[i+1]) or (l[i]<l[i-1] and l[i]<l[i+1]))
       cnt+=1
 print(cnt)
