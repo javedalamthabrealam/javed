142f500e016c0ebbd6c9e758a9065d95293e222b
javedalamthabrealam <51222868+javedalamthabrealam@users.noreply.github.com>
2019-07-17 15:20:09 +0530

    Update ineedlove.py

--- ineedlove.py
+++ ineedlove.py
@@ -1,9 +1,11 @@
-na=int(input())
-li=[int(i) for i in input().split()]
-aa=0
-for i in range(1,na-1):
-    if(li[i]<li[i-1] and li[i]<li[i+1]):
-        aa+=1
-    elif(li[i]>li[i-1] and li[i]>li[i+1]):
-        aa+=1
-print(aa)
+n,m=list(map(int,input().split()))
+l=list(map(int,input().split()))
+l.sort(reverse=True)
+c=0
+for i in l:
+  if m==0:
+    break
+  q1=m//i
+  c+=q1
+  m=m-i*(q1)
+print(c)
